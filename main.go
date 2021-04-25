f4de1557bbbe10589480718307111ef6e7190d90
ricoPi <mail@ri-co.cn>
2021-04-25 18:44:42 +0800

    取消阻塞

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"sync"
-
 	"github.com/sirlanri/iot1-pi/output"
 	"github.com/sirlanri/iot1-pi/server"
 	_ "github.com/sirlanri/iot1-pi/server"
 )
 
 func main() {
-	wait := sync.WaitGroup{}
-	wait.Add(1) //阻塞准备
-
 	//sensor.SendHT()
 	output.LedSwich("on")
 	server.IrisInit()
-	//阻塞
-	wait.Wait()
+
 	return
 }
